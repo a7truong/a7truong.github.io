cea64c71ac4f781c7d2014e97886684d2652b70c
a7truong <andytruong@Andys-MacBook-Pro.local>
2015-04-28 10:50:28 -0400

    Email server testing

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['subject']) 		||
+   empty($_POST['subject'])   ||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -19,8 +19,10 @@ $message = $_POST['message'];
 $to = 'a7truong@uwaterloo.ca'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nSubject: $subject\n\nMessage:\n$message";
-$headers = "From: a7truong@uwaterloo.ca\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+//$headers = "From: andy.truong16@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+//$headers = "Reply-To: $email_address";	
+$from = "From: $name<$email_address>\r\nReturn-path: $email_address";
+mail("a7truong@uwaterloo.ca",$email_subject,$email_body,$from);
+echo "Email Sent!";
 return true;			
 ?>
